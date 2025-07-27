bcf455d58a0c70f52a34e3e9039d9cddd02df1b9
Samriddha <samriddha97@gmail.com>
2025-07-27 23:08:21 +0530

    Empty Project With Only Dependency

---
+++ app/src/main/java/com/example/machinecodingroundapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.machinecodingroundapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.machinecodingroundapp.ui.theme.MachineCodingRoundAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MachineCodingRoundAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "Main Screen",
+        modifier = modifier
+    )
+}
