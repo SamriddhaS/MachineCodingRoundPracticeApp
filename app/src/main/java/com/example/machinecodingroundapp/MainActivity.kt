3e8611ffed4aae65f7ab4505f7299ee72a76be39
SamS <33577968+SamriddhaS@users.noreply.github.com>
2025-08-04 00:30:32 +0530

    Simple Api call with DI,MvvM,Retrofit. (#1)

--- app/src/main/java/com/example/machinecodingroundapp/MainActivity.kt
+++ app/src/main/java/com/example/machinecodingroundapp/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.machinecodingroundapp.ui.theme.MachineCodingRoundAppTheme
+import com.example.machinecodingroundapp.ui.theme.screens.PokemonListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,17 +20,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             MachineCodingRoundAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(modifier = Modifier.padding(innerPadding))
+                    PokemonListScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Main Screen",
-        modifier = modifier
-    )
-}
---
+++ app/src/main/java/com/example/machinecodingroundapp/ui/theme/screens/PokemonListScreen.kt
@@ -0,0 +1,60 @@
+package com.example.machinecodingroundapp.ui.theme.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.machinecodingroundapp.viewmodels.PokemonListViewModel
+
+@Composable
+fun PokemonListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+
+    val pokemonList by viewModel.pokemonList.collectAsState()
+    val error by viewModel.isError.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+
+    Box(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        if (!error.isNullOrEmpty()) {
+            Text("Something went wrong ! ${error}")
+        }else{
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                itemsIndexed(pokemonList){ index,pokemon ->
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(text = "${pokemon.name}")
+                    }
+                    if (index == pokemonList.lastIndex) {
+                        viewModel.loadPokemonWithPagination()
+                    }
+                }
+            }
+            if (isLoading){
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .align(androidx.compose.ui.Alignment.BottomCenter)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
