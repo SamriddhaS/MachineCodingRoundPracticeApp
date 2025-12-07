b27212b509480a7362c06bd7fc7e0d1b25f8d278
Samriddha <samriddha97@gmail.com>
2025-12-07 20:25:07 +0530

    Mythik app - Splash screen updated

--- app/src/main/java/com/example/machinecodingroundapp/ui/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/example/machinecodingroundapp/ui/splash_screen/SplashScreen.kt
@@ -1,13 +1,15 @@
 package com.example.machinecodingroundapp.ui.splash_screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.machinecodingroundapp.R
 import kotlinx.coroutines.delay
 
 @Composable
@@ -15,8 +17,8 @@ fun SplashScreen(
     onNavigateHome: () -> Unit
 ) {
     LaunchedEffect(Unit) {
-        delay(1000) // 1 seconds
-        onNavigateHome()
+        delay(1000)
+            onNavigateHome()
     }
 
     Box(
@@ -25,10 +27,15 @@ fun SplashScreen(
             .background(Color.Black),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "MyThik.AI",
-            color = Color.White,
-            fontSize = 28.sp
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.placeholder_image),
+                contentDescription = "App Logo",
+                modifier = Modifier.width(200.dp).height(150.dp)
+            )
+        }
     }
 }
